73dffd16bb3a8fec4c3acc18993c01915dad9a5d
Timofey Tipishev <tipishev@gmail.com>
2016-07-01 16:11:23 +0500

    use render_to_string in template test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,7 +15,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        content = response.content
-        self.assertTrue(content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', content)
-        self.assertTrue(content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        content = (response.content.decode(), expected_html)
