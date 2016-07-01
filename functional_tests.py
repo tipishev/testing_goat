21335fe448ab1102315aa92bcabfdeec0cf09d81
Timofey Tipishev <tipishev@gmail.com>
2016-07-01 16:41:22 +0500

    create a minimal input page + fix selenium table

--- functional_tests.py
+++ functional_tests.py
@@ -36,9 +36,12 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        rows = self.browser.find_elements_by_tag_name('tr')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item is not in the table"
         )
 
         # There is still a text box inviting her to add another item. She
