d42ec76b11a33e612e6749273dd45526a1e94be7
Timofey Tipishev <tipishev@gmail.com>
2016-07-06 14:47:21 +0500

    add a lists template

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls import include, url
 from lists import views
 
 urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$',
+        views.view_list, name='view_list')
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home_page, name='home')
 ]
