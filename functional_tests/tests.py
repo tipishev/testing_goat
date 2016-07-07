00bcd57ac6f80008ceea634b04377ddeeb1f98e0
Timofey Tipishev <tipishev@gmail.com>
2016-07-07 18:22:30 +0500

    add styling test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,3 +90,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # check the input box on the main is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5,
+        )
+
+        inputbox.send_keys('testing\n')
+
+
+        # check the input box on the new list page is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5,
+        )
+
