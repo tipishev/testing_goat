ca1275108de3257345dfa6aa31278b5ed2dc1bf7
Timofey Tipishev <tipishev@gmail.com>
2016-07-11 23:22:46 +0500

    rename text_input id and add finder-method

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -7,7 +7,7 @@ class LayoutAndStyling(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # check the input box on the main is centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
@@ -17,7 +17,7 @@ class LayoutAndStyling(FunctionalTest):
         inputbox.send_keys('testing\n')
 
         # check the input box on the new list page is centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
