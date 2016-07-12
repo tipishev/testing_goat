fc646c4220e47ff43cc828aa09c0129927104644
Timofey Tipishev <tipishev@gmail.com>
2016-07-12 14:58:05 +0500

    separate tests for Item and List models

--- lists/tests/tests_models.py
+++ lists/tests/tests_models.py
@@ -3,34 +3,19 @@ from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 
-class ListAndItemModelList(TestCase):
+class ItemModelList(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -38,10 +23,6 @@ class ListAndItemModelList(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
-
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -63,6 +44,17 @@ class ListAndItemModelList(TestCase):
         item2 = Item.objects.create(list=list1, text='item 2')
         item3 = Item.objects.create(list=list1, text='3')
         self.assertEqual(
-            Item.objects.all(),
+            list(Item.objects.all()),
             [item1, item2, item3]
         )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
+
+
+class ListModelList(TestCase):
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
