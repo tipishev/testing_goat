0189722bc70e8dc99c7223a5c8b071beeabb0dd2
Timofey Tipishev <tipishev@gmail.com>
2016-07-22 18:11:14 +0500

    fake send_email for testing

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,11 @@ from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email],
+    )
     return redirect('/')
