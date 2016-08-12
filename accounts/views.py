fa10d3aed403daec9a374e575b57890c29191da7
Timofey Tipishev <tipishev@gmail.com>
2016-08-13 02:07:32 +0500

    comment out logging, send generated token in emailt

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-
 from django.core.mail import send_mail
-from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, render
+
+from django.contrib import messages
+
+from accounts.models import Token
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token={uid}'.format(uid=token.uid))
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        'Use this link to log in\n\n{url}'.format(url=url),
         'noreply@superlists',
         [email],
     )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
